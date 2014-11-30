b1bc39328a5fb3dbc8a1f919a6b532979c38b927
Yamashiro <yamashiro@ryukyu-i.co.jp>
2014-11-30 18:13:06 +0900

    Remove unnecessary comment outs

--- app/controllers/project.go
+++ app/controllers/project.go
@@ -15,13 +15,11 @@ func (c Project) List() revel.Result {
     c.Response.ContentType = "application/json; charset=utf8"
 
     if c.Request.Method == "POST" {
-        //b := models.FindProjectListByTeamId(c.Database, "54577d4ce4b0c733f78cb7a7")
         b := models.FindProjectListByTeamId(c.Database, c.Request.PostForm.Get("team_id"))
         Max := len(b) - 1
         return c.Render(b, Max)
     }
  
 	return c.Render()
-    //return c.RenderJson(b)
 }
 
